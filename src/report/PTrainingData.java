544d816038eedc846dcff782a5a92f7192a2676a
StackDev-TCC <tccengenhariacomputacao2020@gmail.com>
2021-12-19 17:45:40 -0300

    Criada a classe PTrainingData

--- src/report/PTrainingData.java
+++ src/report/PTrainingData.java
@@ -1,5 +1,57 @@
 package report;
 
-public class TrainingData {
-    
+public class PTrainingData {
+    long timestamp;
+    int epoch;
+    double newEpochProbability;
+    double[] weightVariationByLayer;
+    double errorSummary;
+
+    public PTrainingData(long timestamp, int epoch, double newEpochProbability, double[] weightVariationByLayer, double errorSummary) {
+        this.timestamp = timestamp;
+        this.epoch = epoch;
+        this.newEpochProbability = newEpochProbability;
+        this.weightVariationByLayer = weightVariationByLayer;
+        this.errorSummary = errorSummary;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getEpoch() {
+        return epoch;
+    }
+
+    public void setEpoch(int epoch) {
+        this.epoch = epoch;
+    }
+
+    public double getNewEpochProbability() {
+        return newEpochProbability;
+    }
+
+    public void setNewEpochProbability(double newEpochProbability) {
+        this.newEpochProbability = newEpochProbability;
+    }
+
+    public double[] getWeightVariationByLayer() {
+        return weightVariationByLayer;
+    }
+
+    public void setWeightVariationByLayer(double[] weightVariationByLayer) {
+        this.weightVariationByLayer = weightVariationByLayer;
+    }
+
+    public double getErrorSummary() {
+        return errorSummary;
+    }
+
+    public void setErrorSummary(double errorSummary) {
+        this.errorSummary = errorSummary;
+    }
 }
